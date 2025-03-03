f90f499634ae0a0b2176854deefcc9d76437c108
Snipebusher <snipebusher@gmail.com>
2025-03-03 12:57:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,21 +11,6 @@ from tkinter import filedialog
 import os
 import subprocess
 
-from lib.pages import handle_new_instance
-
-class CustomRequestHandler(server.RequestHandler):
-    def do_GET(self):
-        if self.path == "/new_instance":
-            subprocess.Popen(["python", "main.py"])
-            self.send_response(200)
-            self.send_header("Content-type", "text/plain")
-            self.end_headers()
-            self.wfile.write(b"New instance started")
-            # IMPORTANT: Return here so we don't call super().do_GET()
-            return
-        else:
-            super().do_GET()
-
 def run_file_dialog():
     root = tk.Tk()
     root.withdraw()
@@ -60,13 +45,13 @@ if __name__ == "__main__":
   if selected_file:
     server.defaultPath = selected_file
   
-  webbrowser.register('opera', None, webbrowser.BackgroundBrowser("S:\\Apps\\Opera GX\\Opera GX\\opera.exe"))
-  webbrowser.get('opera').open("http://localhost:8888")
+  # webbrowser.register('opera', None, webbrowser.BackgroundBrowser("S:\\Apps\\Opera GX\\Opera GX\\opera.exe"))
+  # webbrowser.get('opera').open("http://localhost:8888")
+  webbrowser.open("http://localhost:8888")
 
   httpd = http.server.HTTPServer(server_address, server.RequestHandler)
   thread = threading.Thread(target=httpd.serve_forever, daemon=True)
   thread.start()
-  time.sleep(0.5)
   if thread.is_alive():
     print("listening to", "http://%s:%s/" % server_address)
     if args.open:
